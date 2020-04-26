8530198b3343bed359284b84f30ce773bb764fbd
Navjot-Dhawan <navjotd139@gmail.com>
2020-04-26 22:26:52 +0530

    created route for todo completed or not

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,44 @@
 const router = require('express').Router();
 const Todo = require('../models/todos');
 
-router.get('/',function(req,res){
-    Todo.find({}).then(function(results){
-        res.render('index',{ todos : results });
+router.get('/', function (req, res) {
+    Todo.find({}).then(function (results) {
+        res.render('index', { todos: results });
     });
-    
+
 });
 
-router.post("/todos",function(req,res){
+router.post("/todos", function (req, res) {
     //res.json(req.body);
-    let newTodo = new Todo({description: req.body.description});
-   
-    newTodo.save().then(function(result){
-        console.log(result);
-        res.redirect('/');
-    });
+    let newTodo = new Todo({ description: req.body.description });
+
+    newTodo
+        .save()
+        .then(function (result) {
+            console.log(result);
+            res.redirect('/');
+        })
+        .catch(function (err) {
+            console.log(err);
+            res.redirect('/');
+        });
 });
+
+router.post("/todos/:id/completed",function(req, res){
+
+    let todoId = req.params.id;
+
+    Todo
+    .findById(todoId)
+    .exec()
+    .then(function(result){
+        result.done = !result.done;
+        return result.save();
+    })
+    .then(function(result){
+        res.redirect('/');
+    })
+    
+})
 
 module.exports = router;
